aa844cffd1dbf124079f62d52de0d9ba75200e8c
Rohan Gautam <rohan1gautam@gmail.com>
2024-03-01 13:43:07 +0800

    update metadata

--- _data/metadata.js
+++ _data/metadata.js
@@ -1,11 +1,10 @@
 module.exports = {
 	title: "Rohan's blog",
-	url: "https://example.com/",
+	url: "https://rohangautam.github.io/",
 	language: "en",
 	description: "I am writing about my experiences as a naval navel-gazer.",
 	author: {
-		name: "Your Name Here",
-		email: "youremailaddress@example.com",
-		url: "https://example.com/about-me/",
+		name: "Rohan Gautam",
+		url: "https://rohangautam.github.io/about",
 	},
 };
